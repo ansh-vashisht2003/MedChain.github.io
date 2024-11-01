e7da31000164f9836115c43f928b18ca433b0577
ansh-vashisht2003 <anshvashisht.2003@gmail.com>
2024-11-01 10:19:02 +0530

    Update Medicine.sql

--- Medicine.sql
+++ Medicine.sql
@@ -29,7 +29,25 @@ CREATE TABLE delivery_person (
     phone_number VARCHAR(15) NOT NULL
 );
 
+CREATE TABLE orders (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    request_id INT ,              
+    supplying_chemist_id INT ,  
+    medicine_name VARCHAR(100) ,     
+    quantity INT , 
+    price FLOAT  ,  
+    order_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    status ENUM('Pending', 'Accepted', 'Shipped', 'Delivered', 'Cancelled') DEFAULT 'Pending',
+    requesting_chemist_address VARCHAR(255) ,
+    supplying_chemist_address VARCHAR(255),
+    delivery_person_id INT,              
+    FOREIGN KEY (request_id) REFERENCES requesting_chemist(id),  
+    FOREIGN KEY (supplying_chemist_id) REFERENCES supplying_chemist(id),
+    FOREIGN KEY (delivery_person_id) REFERENCES delivery_person(id)     
+);
+
+DROP TABLE orders;
+select * from orders;
 select * from delivery_person;
 select * from requesting_chemist;
 select * from supplying_chemist;
-
